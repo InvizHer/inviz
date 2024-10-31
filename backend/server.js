9f3cc28c3fc6293f605a15c20c2d1df4def93704
InvizHer <112791490+InvizHer@users.noreply.github.com>
2024-10-31 16:36:45 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+const connectDB = require("./config/db");
+const postsRouter = require("./routes/posts");
+const authRouter = require("./routes/auth");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+connectDB();
+
+// Routes
+app.use("/api/posts", postsRouter);
+app.use("/api/auth", authRouter);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
